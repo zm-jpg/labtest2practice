8033e91eca1c7978839c72be23a4c6414d62e916
ongzongming <zooming.24@ichat.sp.edu.sg>
2025-05-25 21:02:37 +0800

    calc for median

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,4 @@
+import statistics as stats
 
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67,32)")
@@ -18,6 +19,13 @@ def calc_min_max_temperature(numlist):
     maximum=max(numlist)
     min_max=[minimum,maximum]
     return min_max
+
+def calc_median_temperature(numlist):
+    median=stats.median(numlist)
+    return median
+
+
+
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
@@ -26,7 +34,8 @@ def main():
     print("average temp = " +str(average))
     min_max=calc_min_max_temperature(num_list)
     print("min,max = " + str(min_max))
-
+    median=calc_median_temperature(num_list)
+    print("median = " + str(median))
 
 if __name__ == "__main__":
     main()
